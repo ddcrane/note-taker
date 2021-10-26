e7008eaa0173834b41b565c099dd35dfe649a3ed
David Crane <ddcrane2@gmail.com>
2021-10-26 12:25:36 -0600

    uuid

---
+++ db/note-storage.js
@@ -0,0 +1,45 @@
+const util = require('util');
+const fs = require('fs');
+
+
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Storage {
+    read() {
+      return readFileAsync('db/db.json', 'utf8');
+    }
+  
+    write(note) {
+      return writeFileAsync('db/db.json', JSON.stringify(note));
+    }
+  
+    getNotes() {
+      return this.read().then((notes) => {
+        let parsedNotes;
+        try {
+          parsedNotes = [].concat(JSON.parse(notes));
+        } catch (err) {
+          parsedNotes = [];
+        }
+        return parsedNotes;
+      });
+    }
+  
+    addNote(note) {
+      const { title, text } = note;
+ 
+      const newNote = { title, text, id: uuidv4() };
+  
+      return this.getNotes()
+        .then((notes) => [...notes, newNote])
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => newNote);
+    }
+  
+  }
+  
+  module.exports = new Storage();
